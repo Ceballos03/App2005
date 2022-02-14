6dca1e7053c58e7a7b686d1e1c089d7b75737152
Ceballos03 <santiagojoaquinceballos@gmail.com>
2022-02-14 18:36:48 -0300

    componentes2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './componentes/NavBar/NavBar';
 import ItemListContainer from './componentes/ItemListContainer';
+import FunctionCounter from './componentes/FunctionCounter/FunctionCounter';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <ItemListContainer greeting="Hola Mundo" />
+      <FunctionCounter />
     </div>
   );
 }
